e051719ceefe797699509516cf3e432a6c5d525d
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-25 21:24:41 +0000

    function map that goes through an array and callbacks a different function to push into a new array

---
+++ map.js
@@ -0,0 +1,38 @@
+const eqArrays = function (firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let x = 0; x < firstArray.length; x++) {
+    const leftElement = firstArray[x];
+    const rightElement = secondArray[x];
+    if (leftElement !== rightElement) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let x = eqArrays(actual, expected);
+  //console.log(x);
+  if (x === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+console.log(assertArraysEqual(results1, words));
