1a0ee2424c0ded10e9398258e9fe1da3bf98f42c
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-25 04:02:42 +0000

    function finds key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function (tvShows, showName) {
+  const objArray = Object.keys(tvShows);
+  console.log(objArray);
+  for (const i in objArray) {
+    let show = objArray[i];
+    console.log(`${objArray[show]} : ${showName} : ${tvShows[show]} : ${show}`);
+    if (tvShows[show] === showName) {
+      return show;
+    }
+  }
+};
+
+
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
