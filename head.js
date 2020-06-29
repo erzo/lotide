f0c3bb1216899baf5b2c6ea6dae03eaf596ecb3c
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-29 18:55:05 +0000

    refactor code

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    
-  } else if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual');
+
 
 const head = function(firstNum) {
   return firstNum[0];
 }
 
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 3);
-assertEqual(head([]), 3);
+module.exports = head;
