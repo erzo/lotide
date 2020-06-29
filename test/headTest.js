f9c412c6bf87e2ea06df7083293f8f99ce153585
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-29 22:25:02 +0000

    test code for head.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,26 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 3);
-assertEqual(head([]), 3);
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 6 for [6, 2, 7]", () => {
+    assert.strictEqual(head([6,2,7]), 6);
+  });
+
+  it(`returns "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns 3 for [3]", () => {
+    assert.strictEqual(head([3]), 3);
+  });
+
+});
