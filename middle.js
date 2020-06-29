093f24b9a81bb4bfc7f84d8257e0a9aaaf0560d5
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-29 22:24:11 +0000

    refactor code

--- middle.js
+++ middle.js
@@ -1,16 +1,3 @@
-const eqArrays = function (firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let x = 0; x < firstArray.length; x++) {
-    const leftElement = firstArray[x];
-    const rightElement = secondArray[x];
-    if (leftElement !== rightElement) {
-      return false;
-    }
-  }
-  return true;
-};
 const middle = function(array) {
   newArray = [];
   let arrayHalfLength = Math.floor(array.length / 2);
