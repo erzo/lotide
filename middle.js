b6fb750f1e84c88c411553ce85c2b5161943c2e3
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-29 20:39:38 +0000

    refactor code

--- middle.js
+++ middle.js
@@ -11,22 +11,9 @@ const eqArrays = function (firstArray, secondArray) {
   }
   return true;
 };
-
-const assertArraysEqual = function(actual, expected) {
-  let x = eqArrays(actual, expected);
-  //console.log(x);
-  if (x === false) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  
-};
-
 const middle = function(array) {
   newArray = [];
   let arrayHalfLength = Math.floor(array.length / 2);
-  //console.log('array:', arrayHalfLength);
   if (array.length < 3) {
     return newArray;
   }
@@ -39,13 +26,5 @@ const middle = function(array) {
 }
 
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// export middle() function
+module.exports = middle;
