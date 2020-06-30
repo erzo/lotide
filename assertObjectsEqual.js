de0189ed9b84f48137a9d8411930067afe8081c0
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-30 02:27:05 +0000

    refactor code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,22 +1,4 @@
-const eqObjects = function(object1, object2) {
-  const newObject1 = Object.keys(object1);
-  const newObject2 = Object.keys(object2);
-
-  if (newObject1.length !== newObject2.length) {
-    return false;
-  }
-  for (const element in object1) {
-    if (object1[element] !== object2[element]) {
-      //console.log(element);
-        if (Array.isArray(object1[element]) === Array.isArray(object2[element])) {
-          const result = eqArrays(object1[element], object2[element]);
-          return result;
-        }
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 
 const assertObjectsEqual = function(actual, expected) {
@@ -33,4 +15,8 @@ const assertObjectsEqual = function(actual, expected) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(assertObjectsEqual(ab, ba)); // => true
+//console.log(assertObjectsEqual(ab, ba)); // => true
+
+
+// export assertObjectEquals() function
+module.exports = assertObjectsEqual;
