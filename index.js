1c27f6e0ee20b642eae2a0ad5dc7d2d110422e33
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-30 02:40:34 +0000

    index where all functions are within an object

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  assertArraysEqual,
+  assertEqual,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
