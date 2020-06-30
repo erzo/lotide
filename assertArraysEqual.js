113295825141459f3c51c3f3efbb92ab1f246e99
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-06-30 02:26:46 +0000

    refactor code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,5 @@
 const eqArrays = require('./eqArrays');
 
-
-
 const assertArraysEqual = function(actual, expected) {
   let x = eqArrays(actual, expected);
   if (x === false) {
@@ -11,7 +9,6 @@ const assertArraysEqual = function(actual, expected) {
   }
   
 };
-
 
 //exports assertArraysEqual() function
 module.exports = assertArraysEqual;
